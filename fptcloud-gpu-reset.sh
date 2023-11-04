875e1d69c63ffdf908c5d3bd8bd092472cd8e53b
fci-xplat <115626814+fci-xplat@users.noreply.github.com>
2023-11-05 03:29:11 +0700

    Update fptcloud-gpu-reset.sh

--- fptcloud-gpu-reset.sh
+++ fptcloud-gpu-reset.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 echo "*** FPT Cloud - Kubernetes Engine GPU reset ***"
-#remove module gpu
-echo "*Remove gpu modules: "
-nvidia_mod=$(lsmod | grep nvidia)
-if [[ -z "$nvidia_mod" ]]; then
-     echo "No nvidia modules load"
-else
-     rmmod -f nvidia_uvm nvidia_drm nvidia_modeset nvidia
-     if [ $? -eq 0 ] ; then
-          echo "All modules had been removed"
-     else
-          echo "Modules were not removed"
-     fi
-fi
-#reset gpu
-echo "*Check mig instance: " && nvidia-smi -L
-echo "*Reset gpu: " && nvidia-smi --gpu-reset
 #kill gpu process
 echo "*Kill gpu processes are running: "
 PIDS=$(lsof -t /dev/nvidia0)
@@ -27,6 +11,31 @@ else
      kill -9 $PID
      done
 fi
+#remove module gpu
+echo "*Remove gpu modules: "
+nvidia_mod=$(lsmod | grep nvidia)
+if [[ -z "$nvidia_mod" ]]; then
+     echo "No nvidia modules load"
+else
+     rmmod -f nvidia_uvm nvidia_drm nvidia_modeset nvidia
+     if [ $? -eq 0 ] ; then
+          echo "All modules had been removed"
+          module_status=successfull
+     else
+          echo "Modules were not removed"
+          module_status=failed
+     fi
+fi
+#reset gpu
+echo "*Check mig instance: " && nvidia-smi -L
+echo "*Reset gpu: " && nvidia-smi --gpu-reset
+if [ $? -eq 0 ] ; then
+     echo "GPU was reset succeeded"
+     reset_status=successfull
+else
+     echo "GPU was reset failed"
+     reset_status=failed
+fi
 #enable mig
 echo "*Enable mig: "
 mig_status=$(nvidia-smi -i 0 --query-gpu=mig.mode.current --format=csv,noheader)
@@ -36,5 +45,8 @@ else
      nvidia-smi -i 0 -mig 1
 fi
 #reset result
-echo "*GPU reset result: " && nvidia-smi -L
+echo "=== GPU reset result==="
+echo "*GPU instances: " && nvidia-smi -L
 echo "*GPU processes are running: " && fuser -v /dev/nvidia* && ps -ef | grep nvidia
+echo "*GPU remove modules: $module_status"
+echo "*GPU reset status: $reset_status"
