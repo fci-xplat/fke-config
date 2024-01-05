4c98e5a8b68fab39d6bdc6a7b06c2b5729a1410e
vinhvn4 <vinhvn4@fpt.com>
2024-01-05 15:29:09 +0700

    Add env toolkit

--- fptcloud-gpu-driver-installer.sh
+++ fptcloud-gpu-driver-installer.sh
@@ -9,6 +9,7 @@ set -u
 set -x
 NVIDIA_DRIVER_BRANCH="XFree86"
 NVIDIA_DRIVER_VERSION="${NVIDIA_DRIVER_VERSION:-510.108.03}"
+NVIDIA_TOOLKIT_INSTALL="${NVIDIA_TOOLKIT_INSTALL:-true}"
 NVIDIA_DRIVER_DOWNLOAD_URL_DEFAULT="https://download.nvidia.com/${NVIDIA_DRIVER_BRANCH}/Linux-x86_64/${NVIDIA_DRIVER_VERSION}/NVIDIA-Linux-x86_64-${NVIDIA_DRIVER_VERSION}.run"
 NVIDIA_DRIVER_DOWNLOAD_URL="${NVIDIA_DRIVER_DOWNLOAD_URL:-$NVIDIA_DRIVER_DOWNLOAD_URL_DEFAULT}"
 NVIDIA_INSTALL_DIR_HOST="${NVIDIA_INSTALL_DIR_HOST:-/var/lib/nvidia}"
@@ -19,6 +20,7 @@ CACHE_FILE="${NVIDIA_INSTALL_DIR_CONTAINER}/.cache"
 KERNEL_VERSION="$(uname -r)"
 NVIDIA_TOOLKIT_LIB_URL="https://nvidia.github.io/libnvidia-container/gpgkey"
 NVIDIA_TOOLKIT_DOWNLOAD_URL="https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list"
+CONFIG_NVIDIA_CONTAINERD_DOWNLOAD_URL="https://raw.githubusercontent.com/fci-xplat/fke-config/ADD/install-toolkit/config.toml"
 set +x
 
 check_cached_version() {
@@ -138,7 +140,7 @@ install_nvidia_toolkit() {
 
 change_default_container_runtime() {
   echo "Changing config default container runtime to nvidia..."
-  curl -Ls https://raw.githubusercontent.com/fci-xplat/fke-config/ADD/install-toolkit/config.toml > "/etc/containerd/config.toml"
+  curl -Ls ${CONFIG_NVIDIA_CONTAINERD_DOWNLOAD_URL} > "/etc/containerd/config.toml"
   echo "Change config container run time... DONE"
   systemctl restart containerd
 }
@@ -154,17 +156,19 @@ main() {
   if check_cached_version; then
     #configure_cached_installation
     verify_nvidia_installation
-    install_nvidia_toolkit
-    change_default_container_runtime
   else
     download_kernel_src
     configure_nvidia_installation_dirs
     download_nvidia_installer
     run_nvidia_installer
     update_cached_version
+    if ${NVIDIA_TOOLKIT_INSTALL}; then
+      echo "Installing NVIDIA TOOLKIT..."
+      install_nvidia_toolkit
+      change_default_container_runtime
+    else
+      echo "Ignore NVIDIA TOOLKIT..."
     verify_nvidia_installation
-    install_nvidia_toolkit
-    change_default_container_runtime
   fi
   clean_nvidia_installation
   # update_host_ld_cache
