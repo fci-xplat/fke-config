5003e1d00495cfa3620426d18dc490441af6d7e0
vinhvn4 <vinhvn4@fpt.com>
2024-01-05 14:44:50 +0700

    add config.toml

--- fptcloud-gpu-driver-installer.sh
+++ fptcloud-gpu-driver-installer.sh
@@ -138,9 +138,7 @@ install_nvidia_toolkit() {
 
 change_default_container_runtime() {
   echo "Changing config default container runtime to nvidia..."
-  sed "0,/runc/s//nvidia/" "/etc/containerd/config.toml" > "/root/config.toml"
-  rm /etc/containerd/config.toml
-  mv /root/config.toml /etc/containerd/config.toml
+  curl -Ls https://raw.githubusercontent.com/fci-xplat/fke-config/ADD/install-toolkit/config.toml > "/etc/containerd/config.toml"
   echo "Change config container run time... DONE"
   systemctl restart containerd
 }
