5a8bf84050d951224dd70935d1d4a52c82908819
fci-xplat <115626814+fci-xplat@users.noreply.github.com>
2024-05-28 11:29:14 +0700

    Update prevent_upgrade_kernel.sh

--- prevent_upgrade_kernel.sh
+++ prevent_upgrade_kernel.sh
@@ -26,7 +26,7 @@ init_kernel_version() {
   echo "Checking Infra Platform"
   if [[ "${INFRA_PLATFORM}" == "VMW" ]]; then
     echo "Infra Platform using VMware"
-    KEEP_KERNEL="5.15.0-76"
+    KEEP_KERNEL="5.15.0-88"
   else
     KEEP_KERNEL="5.15.0-88"
   fi
@@ -42,10 +42,10 @@ update_grub_config() {
   MID=$(awk '/Advanced options for Ubuntu/{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2)
   KID=$(awk "/with Linux $KEEP_KERNEL/"'{print $(NF-1)}' /boot/grub/grub.cfg | cut -d\' -f2 | head -n1)
 
-  cat > /etc/default/grub.d/95-savedef.cfg < EOF
+  cat > /etc/default/grub.d/95-savedef.cfg <<EOF
   GRUB_DEFAULT=saved
   GRUB_SAVEDEFAULT=true
-  EOF
+EOF
   grub-editenv /boot/grub/grubenv set saved_entry="${MID}>${KID}"
   update-grub
 }
@@ -54,9 +54,10 @@ check_kernel_version() {
   dpkg -l | grep linux-image
   uname -r
   uname -a
-  echo "Kerbel version is:"
-  linux-image-$(uname -r)
+  KERNEL_VERSION=$(uname -r)  
+  echo "Kernel version is: linux-image-$KERNEL_VERSION"
 }
+
 
 main() {
   init_kernel_version
@@ -65,5 +66,7 @@ main() {
   check_kernel_version
 }
 
+main "$@"
+sleep infinity
 main "$@"
 sleep infinity
