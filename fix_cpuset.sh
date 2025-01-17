5987a8b31fed997c4ca79d22f4ebe652157bb583
fci-xplat <115626814+fci-xplat@users.noreply.github.com>
2025-01-17 10:49:55 +0700

    Update fix_cpuset.sh

--- fix_cpuset.sh
+++ fix_cpuset.sh
@@ -1,26 +1,43 @@
 #!/bin/bash
 
 check_cpuset() {
+    echo "Node:"
     echo $(cat /etc/hostname)
+    echo "cpuset/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus)
+    echo "kube.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kube.slice/cpuset.cpus)
+    echo "kube.slice/cpuset.effective_cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kube.slice/cpuset.effective_cpus)
+    echo "kube.slice/kubelet.service/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kube.slice/kubelet.service/cpuset.cpus)
+    echo "kube.slice/kubelet.service/cpuset.effective_cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kube.slice/kubelet.service/cpuset.effective_cpus)
+    echo "kubepods.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/cpuset.cpus)
+    echo "kubepods.slice/cpuset.effective_cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/cpuset.effective_cpus)
+    echo "kubepods.slice/kubepods-besteffort.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-besteffort.slice/cpuset.cpus)
+    echo "kubepods.slice/kubepods-besteffort.slice/cpuset.effective_cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-besteffort.slice/cpuset.effective_cpus)
+    echo "kubepods.slice/kubepods-burstable.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-burstable.slice/cpuset.cpus)
+    echo "kubepods.slice/kubepods-burstable.slice/cpuset.effective_cpus"
     echo $(cat /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-burstable.slice/cpuset.effective_cpus)
 }
 
 fix_cpuset() {
     echo "Difference detected between the cpuset.cpus files."
+    echo "kubepods.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus) > /sys/fs/cgroup/cpuset/kubepods.slice/cpuset.cpus
+    echo "kube.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus) > /sys/fs/cgroup/cpuset/kube.slice/cpuset.cpus
+    echo "kube.slice/kubelet.service/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus) > /sys/fs/cgroup/cpuset/kube.slice/kubelet.service/cpuset.cpus
+    echo "kubepods.slice/kubepods-besteffort.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus) > /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-besteffort.slice/cpuset.cpus
+    echo "kubepods.slice/kubepods-burstable.slice/cpuset.cpus"
     echo $(cat /sys/fs/cgroup/cpuset/cpuset.cpus) > /sys/fs/cgroup/cpuset/kubepods.slice/kubepods-burstable.slice/cpuset.cpus
 }
 
